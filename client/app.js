5da7dd573ed8bdc2837aaf2e8c8af4de9b6c4ac3
Kai Davenport <kaiyadavenport@gmail.com>
2017-01-13 00:41:32 +0000

    non state version with k8s logo

--- client/app.js
+++ client/app.js
@@ -2,57 +2,8 @@
 
   var width = 110
   var height = 100
-  var connectionStatus = false
-  var dataLoaded = false
-
-  function activate_page(){
-    if(!dataLoaded){
-      dataLoaded = true
-      load_data(build_page)
-    }
-    $('#preloader').hide()
-  }
-
-  function disable_page(){
-    $('#preloader').show()
-  }
-
-  function load_data(done){
-    $.get('/v1/whales', function(data){
-
-      if(typeof(data)=='string') data = JSON.parse(data)
-
-      var ret = data.map(function(st){
-        var parts = st.split(':')
-        return {
-          x:parts[0],
-          y:parts[1]
-        }
-      })
-
-      done(ret)
-    
-    })
-  }
-
-  function add_data(x, y, done){
-    $.post('/v1/whales', x + ':' + y, done)
-  }
-
-  function check_status(done){
-    $.get('/v1/ping', function(data){
-      connectionStatus = data.connected
-
-      if(connectionStatus){
-        activate_page()
-      }
-      else{
-        disable_page()
-      }
-
-      setTimeout(check_status, 1000)
-    })
-  }
+  var connectionStatus = true
+  var dataLoaded = true
 
   function add_whale(x, y, animate){
     var holder = document.getElementById('holder')
@@ -70,27 +21,17 @@
     holder.appendChild(elem)
   }
 
-  // will put the mobies on the screen as per state
-  function build_page(data){
-    data.forEach(function(pos){
-      add_whale(pos.x, pos.y)
-    })
-  }
-
   function handle_click(e){
     if(!connectionStatus) return
     var offset = $(this).offset();
     var x = e.pageX - offset.left - (width/2);
     var y = e.pageY - offset.top - (height/2);
-    add_data(x, y, function(){
-      add_whale(x, y, true)
-    })
+    add_whale(x, y, true)
   }
 
   $(function(){
-    $('#preloader').show()
     $('#holder').click(handle_click)
-    check_status()
+    $('#preloader').hide()
   })
 
 })()
--- server.js
+++ server.js
@@ -9,46 +9,9 @@ module.exports = function(opts){
 
   var connectionStatus = true
 
-  function readData(){
-    var whaleData = fs.readFileSync(dataFile, 'utf8')
-    return JSON.parse(whaleData)
-  }
-
-  function writeData(data){
-    fs.writeFileSync(dataFile, JSON.stringify(data), 'utf8')
-  }
-
-  if(!fs.existsSync(dataFile)){
-    writeData([])
-  }
 
   var router = Router()
   var fileServer = ecstatic({ root: __dirname + '/client' })
-
-  router.addRoute("/v1/ping", {
-    GET: function(req, res){
-      res.setHeader('Content-type', 'application/json')
-      res.end(JSON.stringify({
-        connected:connectionStatus
-      }))
-    }
-  })
-
-  router.addRoute("/v1/whales", {
-    GET: function (req, res) {
-      var whaleData = JSON.stringify(readData())
-      res.end(whaleData)
-    },
-    POST: function (req, res) {
-      req.pipe(concat(function(data){
-        data = data.toString()
-        var allWhales = readData()
-        allWhales.push(data)
-        writeData(allWhales)
-        res.end('ok')
-      }))
-    }
-  })
 
   router.addRoute("/*", fileServer)
 
